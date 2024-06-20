76f20afc01ddc3b978e5d3be42ab9c02271223bb
SysAdmin MichelV69 <902pe_gaming@wolfstar.ca>
2024-06-20 15:06:50 -0300

    Starting to define basic Struct / Implements based on the original code.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,58 @@
-use mini_redis::{client, Result as redis_Result};
-use color_eyre::eyre::Result;
-use tracing::info;
-use std::collections::HashMap;
-use rayon::prelude::*;
-use chrono::prelude::{DateTime, Local};
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+
+//use mini_redis::{client, Result as redis_Result};
+//use color_eyre::eyre::Result;
+//use tracing::info;
+//use std::collections::HashMap;
+//use rayon::prelude::*;
+//use chrono::prelude::{DateTime, Local};
+
+// extern crate inquire;
+
+use strum_macros::Display;
+use strum_macros::EnumString;
+use std::str::FromStr;
+use inquire::Text;
+use rand_derive2::RandGen;
 
 fn main() {
     println!("Hello, world!");
-};
+
+    #[derive(Debug)]
+    struct EstablishmentQuality
+    {
+        quality : String,
+        rooms  : String,
+        meals : String,
+    }
+
+    #[derive(Debug)]
+    struct PBHouse {
+        name: String,
+        mood: String,
+        lighting: String,
+        smells: String,
+        size: String,
+        posted_sign: String,
+        specialty_drink: String,
+        specialty_food: String,
+        establishment_history_notes: String,
+        redlight_services: String,
+        establishment_quality: EstablishmentQuality,
+        cost_of_goods_index:String
+    }
+
+    enum NameVerb{
+        Waltzing,Checkered,Lazy,Silver,Saucy,Flirting
+    }
+    enum NameNoun {
+        Werebear,Cockrel,Hen,Dragon,Wench,Dryad
+    }
+
+    impl PBHouse {
+
+    }
+}
